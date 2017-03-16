6e386d3ec0cfaa9ba2789b074596b38648de4c20
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-03-16 12:44:12 +0800

    Read_CSV

---
+++ 01Basic/76Read_CSV.R
@@ -0,0 +1,14 @@
+
+data <- read.table("Desktop/data.csv", header = TRUE, sep = ",") 
+
+# col.names: set column name¡C
+data <- read.table("Desktop/data.csv", header = TRUE, sep = ",", col.names = c("Time", "Title")) 
+
+# skip some data
+data <- read.table("Desktop/data.csv", header = FALSE, sep = ",", skip = 10) 
+
+# encoding: UTF-8
+data <- read.table("Desktop/data.csv", header = TRUE, sep = ",", encoding = "UTF-8") 
+
+# na.strings: change NA¡C
+data <- read.table("Desktop/data.csv", header = TRUE, sep = ",", na.strings = NA) 
