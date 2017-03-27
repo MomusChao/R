0cd113deaf17c51ffac34aae68a819c149943339
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-03-27 21:08:53 +0800

    Recode__01

---
+++ 01Basic/98Recode__01.R
@@ -0,0 +1,6 @@
+score = c(25,14,20,69,82,20,10,33,52,19,50)
+
+newLabel = c("Low","Middle","High")
+score2 = 1*(score<=20)+2*(score<=40 & score>20)+3*(score>40)
+newScore = newLabel[score2]
+newScore
