8b70bbc2993d6fa4a8d913009f4605ce9d8d3a9a
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-03-29 23:09:05 +0800

    Cast: subset

---
+++ 02Advance/125Cast__subset.R
@@ -0,0 +1,15 @@
+#create data.frame
+library(reshape)
+
+ID_A = c("A902","A903","A904","A905")
+Sex = c("male","Female","male","Female")
+ExamTime = c(2000,2010,2000,2000)
+Score = c(81,76,66,70)
+
+
+Score_wide = data.frame(ID_A,Sex,ExamTime,Score)
+Score_wide
+
+
+cast(Score_wide, ID_A ~ ExamTime)
+cast(Score_wide, ID_A ~ ExamTime, subset = ExamTime =="2000")
