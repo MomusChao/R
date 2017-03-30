24024a34c5e092e33f408e5783b06bc1321ffa73
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-03-30 13:48:32 +0800

    Combine: merge with one side

---
+++ 02Advance/131Combine__merge_with_one_side.R
@@ -0,0 +1,19 @@
+# Create data 1
+ID = c("A125","A126","A127")
+English_score = c(88,57,66)
+
+X1 = cbind(ID,English_score)
+X1 = as.data.frame(X1)
+X1
+
+# Create data 2
+ID = c("A125","A126","A127","A128")
+Math_score = c(75,87,91,89)
+
+X2 = cbind(ID,Math_score)
+X2 = as.data.frame(X2)
+X2
+
+# Merge with all data
+X3 = merge(X1,X2,all.x=T)
+X3
