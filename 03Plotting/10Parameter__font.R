ed88e7601c1d8d24972c12973b73cb7b4caad47d
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-04-10 17:31:50 +0800

    Parameter: font

---
+++ 03Plotting/10Parameter__font.R
@@ -0,0 +1,8 @@
+Height = c(170,169,182,175,173)
+Weight = c(66,59,72,78,90)
+
+# choose font
+# 1:normal 2:Bold 3:Italic 4:Bold italic
+plot(Height,Weight,font=2)
+
+#other parameter: font.axis  font.lab font.main  font.sub
