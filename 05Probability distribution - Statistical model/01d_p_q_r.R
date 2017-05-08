bcd8dea21a2aabf560d28a522e21b3ac66f031e6
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-05-08 20:20:32 +0800

    d p q r

---
+++ 05Probability distribution - Statistical model/01d_p_q_r.R
@@ -0,0 +1,14 @@
+# d: density function
+dchisq(2.5,10)
+
+# p: probability (CDF)
+ppois(3,2)
+
+pnorm(1.96,lower.tail = F)
+
+# q: quantile
+qnorm(0.88)
+qnorm(0.05,lower.tail = F)
+
+# r: random number generator
+rbinom(50,10,0.2)
